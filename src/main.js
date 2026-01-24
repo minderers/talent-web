3669a9e378933ffb45977acb5e18a993e53ec129
minder <2210151384@qq.com>
2026-01-24 16:21:48 +0800

    feat: 添加TailwindCSS和修改README

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './style.css'
 const app = createApp(App)
 const store = Pinia.createPinia()
 // 关键代码
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Home</div>
+  <div class="text-3xl font-bold underline">Home</div>
 </template>
 
 <script setup></script>
